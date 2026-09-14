4ff145821477c2a26f2bad86ea8dda56bb63ccf3
kurgeri <b3kurger@vasvari.org>
2026-09-14 11:57:23 +0200

    6. feldata fixed

--- stringpeldak.js
+++ stringpeldak.js
@@ -1,5 +1,5 @@
 //0.Feladat
-let txt = "AbCdeFgHiJkL";
+let txt = "ABcdeFgHiJkL";
 console.log(txt.length);
 
 //1.feladat
@@ -16,13 +16,15 @@ let txtUp = txt.toUpperCase();
 console.log(txtUp);
 
 //6. Feladat
-//to-do
-
-for (let i = 0; i > txt.length; i++) {
+let txt_2_up = "";
+for (let i = 0; i < txt.length; i++) {
   if (i % 2 == 0) {
-    txt[i].toUpperCase();
+    txt_2_up += txt[i].toUpperCase();
+  } else {
+    txt_2_up += txt[i];
   }
 }
+console.log(txt_2_up);
 //7. Feldat
 let txt_e = txt.replaceAll("e", "E");
 console.log(txt_e);
