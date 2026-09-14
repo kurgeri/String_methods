7a9359ec2f217c02f230a24999ad91c2c7e88ee0
kurgeri <b3kurger@vasvari.org>
2026-09-14 09:39:59 +0200

    6-8. feladat

--- stringpeldak.js
+++ stringpeldak.js
@@ -1,17 +1,32 @@
 //0.Feladat
-let txt = "AbCDeFgHiJkL";
+let txt = "AbCdeFgHiJkL";
 console.log(txt.length);
 
 //1.feladat
 console.log(txt.substring(0, 5));
 
 //2. Feldat
-console.log(txt.substring(3, 8));
+console.log(txt.substring(2, 8));
 //3. Feladat
-console.log(txt.substring(5));
+console.log(txt.substring(4));
 //4. Feladat
-console.log(txt.substring(5, 5 + 6));
+console.log(txt.substring(4, 4 + 6));
 //5. Feladat
-console.log(txt.toUpperCase());
+let txtUp = txt.toUpperCase();
+console.log(txtUp);
 
 //6. Feladat
+//to-do
+
+for (let i = 0; i > txt.length; i++) {
+  if (i % 2 == 0) {
+    txt[i].toUpperCase();
+  }
+}
+//7. Feldat
+let txt_e = txt.replaceAll("e", "E");
+console.log(txt_e);
+
+//8.Feladat
+let array = txt.split("e");
+console.log(array);
