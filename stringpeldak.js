11562cd36800f50e8dac5ed0c48b62b2e2639c52
kurgeri <b3kurger@vasvari.org>
2026-09-14 09:30:12 +0200

    1-5 feladatok

--- stringpeldak.js
+++ stringpeldak.js
@@ -1,5 +1,17 @@
-import input from "../input.js";
-
 //0.Feladat
-let txt = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+let txt = "AbCDeFgHiJkL";
 console.log(txt.length);
+
+//1.feladat
+console.log(txt.substring(0, 5));
+
+//2. Feldat
+console.log(txt.substring(3, 8));
+//3. Feladat
+console.log(txt.substring(5));
+//4. Feladat
+console.log(txt.substring(5, 5 + 6));
+//5. Feladat
+console.log(txt.toUpperCase());
+
+//6. Feladat
